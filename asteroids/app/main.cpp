147d9243a6bb4e3f8dce23495f6172713c86b5bd
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-09-30 19:57:46 -0400

    scaffold for project

---
+++ asteroids/app/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <raylib.h>
+
+int main() {
+  InitWindow(600, 600, "C++ - Asteroids");
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
+}
